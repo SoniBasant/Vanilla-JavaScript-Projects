c3afe86280e8d1bcc556bfadb0b6f3cb1d99cd23
SoniBasant <94775800+SoniBasant@users.noreply.github.com>
2022-11-15 10:09:47 +0530

    Add files via upload
    This is a to-do list project. You can enter a task and click to add button, your task will be added at the bottom. Task have two options. One to click on done with the symbol of tick mark. This will strike out the task but it will still be there. Other button is to delete the task. Copyright of the picture belongs to me.

---
+++ To do list/Project 5.js
@@ -0,0 +1,88 @@
+/*
+key concepts for project
+1. document.getElementById()
+2. addEventListener()
+3. e.target
+4. appendChild()
+5. createElement()
+6. parentElement()
+7. remove()
+8. classList.add()
+*/
+
+// variables
+
+const addTask = document.getElementById('add-task'); //button
+const taskContainer = document.getElementById('task-container'); //container
+const inputTask = document.getElementById('input-task'); //input text
+
+// event listener for add button
+
+addTask.addEventListener('click', function(){
+    // create div of task
+    let task = document.createElement('div');
+    // add the style of 'task' in task
+    task.classList.add('task');
+
+    // create list item for the div
+    let li = document.createElement('li');
+    // take the text of list item
+    li.innerText = (inputTask.value);
+    // add list item text to task
+    task.appendChild(li);
+
+    // add check icon button for list item
+    // create check button
+    let checkButton = document.createElement("button");
+    // icon for check
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    // give this icon the style of 'checkTask'
+    checkButton.classList.add('checkTask');
+    // add this icon to the task
+    task.appendChild(checkButton);
+
+    // create delete button
+    let deleteButton = document.createElement("button");
+    // icon for delete button
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    // give delete icon the style of 'deleteTask'
+    deleteButton.classList.add('deleteTask');
+    // add delete button to the task
+    task.appendChild(deleteButton);
+
+    // condition to check for empty entry
+    if(inputTask.value === "") {
+        // message to enter some task if it is not present
+        alert('Please Enter a task');
+    } else {
+        // add that task to task container
+        taskContainer.appendChild(task);
+    }
+
+    // after adding, make you input empty
+    inputTask.value = "";
+
+
+    // event listener for icons
+    // line-through functionality for check button
+    checkButton.addEventListener('click', function() {
+        checkButton.parentElement.style.textDecoration = "line-through";
+    });
+
+    // deleting the task functionality for delete button
+    deleteButton.addEventListener('click', function(e){
+        // to select the element that trigger the event
+        let target = e.target;
+
+        // if we clicked on button
+        if(target.matches('button')) {
+            target.parentElement.remove();
+        }
+
+        // if we clicked on icon
+        if(target.matches('i')) {
+            target.parentElement.parentElement.remove();
+        }
+    });
+
+});
