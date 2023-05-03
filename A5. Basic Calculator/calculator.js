6b3b07bc5fa192fa582e6f80a0db6a785c32d8fc
Basant Soni <94775800+SoniBasant@users.noreply.github.com>
2023-05-03 15:53:26 +0530

    Delete code which is commented and unnecessary

--- A5. Basic Calculator/calculator.js
+++ A5. Basic Calculator/calculator.js
@@ -1,5 +1,5 @@
 
-
+// variables
 // select output of calculator
 const output = document.getElementById("output");
 // select form to make it by id to stop it submitting everytime you press button
@@ -8,16 +8,13 @@ const form = document.getElementById("cal-container");
 const numBtns = document.querySelectorAll("button[data-type=operand]");
 // select all buttons of operator (maths commands) and put them in NodeList (array with node items)
 const symbolBtns = document.querySelectorAll("button[data-type=operator]");
-
+// equal button
 const equalBtn = document.querySelector("button[data-type=result]");
 
 // function to prevent the form from submitting whenever we click a button
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
-
-let is_operator = false;
-let equation = [];
 
 // to remove active class from the button we clicked
 const remove_active = () => {
@@ -26,83 +23,7 @@ const remove_active = () => {
   });
 };
 
-/*
-// iterate over the array of buttons of digits using forEach loop
-numBtns.forEach((btn) => {
-  // add a click event to each button
-  btn.addEventListener("click", (e) => {
-    remove_active();
-    // display operand's value
-    if (output.value == "0") {
-      output.value = e.target.value;
-    } else if (output.value.includes(".")) {
-      // if decimal in our output then stop adding further decimal point by replacing it with empty string
-      output.value = output.value + "" + e.target.value.replace(".", "");
-    } else if (is_operator) {
-      // if user already pressed any operator button, 
-      // set is_operator to false and restart the value in the output from the new value
-      is_operator = false;
-      output.value = e.target.value;
-    } else {
-      // lastly, add more numbers with empty string
-      output.value = output.value + "" + e.target.value;
-    }
-  });
-});
-
-// iterate over the array of buttons of operator using forEach loop 
-symbolBtns.forEach((btn) => {
-  // add a click event to each button
-  btn.addEventListener("click", (e) => {
-    // remove active class from previously active button
-    remove_active();
-    // add active class to the button user click
-    e.currentTarget.classList.add("active");
-
-  // place value of button in switch and check it for each case
-    switch (e.target.value) {
-      // if button is %, output will be->
-      case "%":
-        output.value = parseFloat(output.value) / 100;
-        break;
-
-      // if button is invert, output will be multiplyed by (-1) ->
-      case "invert":
-        output.value = parseFloat(output.value) * -1;
-        break;
-
-      // if button is =, push the last value from the output value to our eqation array
-      case "=":
-        equation.push(output.value);
-        // use eval() to evaluate every eqation
-        output.value = eval(equation.join(""));
-        // then clear the eqation array for further use
-        equation = [];
-        break;
-
-      
-      default:
-        // obtain the last item in the array
-        let lastItem = equation[equation.length - 1];
-        // if button is from top four maths coperator
-        if (["/", "*", "+", "-"].includes(lastItem) && is_operator) {
-          // delete it from the equation
-          equation.pop();
-          // add another value
-          equation.push(e.target.value);
-        } else {
-          // if last array is not operator, add the output value and the value of the button
-          equation.push(output.value);
-          equation.push(e.target.value);
-        }
-        // set the value of operator to true for everytime we click operator button
-        is_operator = true;
-        break;
-    }
-  });
-});
-
-*/
+// function for numbers
 numBtns.forEach((btn) => {
   btn.addEventListener('click', (e) =>{
     remove_active();
@@ -112,15 +33,14 @@ numBtns.forEach((btn) => {
       output.value = e.target.value;
     } else {
       output.value = output.value + e.target.value;
-      console.log(parseFloat(output.value));
     }
   });
 });
 
+// function for symbols
 symbolBtns.forEach((btn) => {
   btn.addEventListener('click', (e) =>{
     remove_active();
-    // console.log(e.target.value);
     output.value = output.value + e.target.value;
 
     switch (e.target.value) {
@@ -133,8 +53,6 @@ symbolBtns.forEach((btn) => {
       case "invert":
         output.value = parseFloat(output.value) * -1;
         break;
-
-      
     }
     
   });
